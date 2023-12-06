da56e1955aebd2783bb007d1c3b54234f27b2716
Buck Brady <bbrady145@gmail.com>
2023-12-05 22:57:04 -0700

    add inkline framework

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import Chakra, { CThemeProvider } from '@chakra-ui/vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+
+import { Inkline, components } from '@inkline/inkline';
+import './css/variables/index.scss';
+import '@inkline/inkline/css/index.scss';
+import '@inkline/inkline/css/utilities.scss';
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-app.use(Chakra)
+app.use(Inkline, { components });
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import inkline from '@inkline/plugin/vite';
+import type { UserOptions } from '@inkline/plugin';
+
+const inklineConfig: UserOptions = {
+    outputDir: 'src/css/variables'
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [inkline(inklineConfig), vue(), vueJsx()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
-})
+});
